98042ecfa562ba055d32ba8fa872e3854032e6bf
luccasfto <luccasfto@gmail.com>
2021-04-12 17:08:27 -0300

    adicionando router para components do header

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './components/views/cadastrar/cadastrar.component';
+import { EntrarComponent } from './components/views/entrar/entrar.component';
+import { EntretenimentoComponent } from './components/views/entretenimento/entretenimento.component';
 import { HomeComponent } from './components/views/home/home.component';
+import { PromocoesComponent } from './components/views/promocoes/promocoes.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'entretenimento',
+    component: EntretenimentoComponent,
+  },
+  {
+    path: 'promocoes',
+    component: PromocoesComponent,
+  },
+  {
+    path: 'cadastrar',
+    component: CadastrarComponent,
+  },
+  {
+    path: 'entrar',
+    component: EntrarComponent,
+  },  
 ];
 
 @NgModule({
@@ -14,3 +34,4 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+
