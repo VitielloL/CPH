f8382accbe88e530f29fe623ad766eaad4a042b8
VitielloL <lucasvitielloo@gmail.com>
2021-04-13 23:48:58 -0300

    colocando redes sociais no footer e realocando botões no header

--- front/src/app/components/template/footer/footer.component.ts
+++ front/src/app/components/template/footer/footer.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faInstagram,
+  faInstagramSquare,
+  faDiscord,
+  faFacebook,
+  faTwitch,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  faInstagramSquare = faInstagramSquare;
+  faInstagram = faInstagram;
+  faDiscord = faDiscord;
+  faFacebook = faFacebook;
+  faTwitch = faTwitch;
+  faTwitter = faTwitter;
+  faYoutube = faYoutube;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
